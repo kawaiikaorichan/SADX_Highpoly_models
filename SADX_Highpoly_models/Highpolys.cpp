909917311b070df0f30c6c5f86e9f5b34365db72
Kaori <super_so_nic1@hotmail.com>
2020-10-27 21:01:12 -0600

    Add project files.

---
+++ SADX_Highpoly_models/Highpolys.cpp
@@ -0,0 +1,14 @@
+#include "SADXModLoader.h"
+#include <stdio.h>
+
+//Macros
+#define ReplacePVM(a, b) helperFunctions.ReplaceFile("system\\" a ".PVM", "system\\" b ".PVM");
+
+extern "C"
+{
+	__declspec(dllexport) __declspec(dllexport) void __cdecl Init(const char* path, const HelperFunctions& helperFunctions)
+	{
+	ReplacePVM("sonic", "sonicnm");
+	}
+	__declspec(dllexport) ModInfo SADXModInfo = { ModLoaderVer };
+}
